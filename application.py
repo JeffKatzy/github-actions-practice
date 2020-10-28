71e82a8d855dff5869fcea1651ba70e231179627
Jeff Katz <JeffreyEricKatz@gmail.com>
2020-10-28 13:38:59 -0400

    change text to friend

--- application.py
+++ application.py
@@ -5,7 +5,7 @@ application = Flask(__name__)
 @application.route("/")
 def hello():
     print('hola world')
-    return "Hello World!"
+    return "Hello Friend!"
 
 # run the app.
 if __name__ == "__main__":
